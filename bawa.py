be4da558c74fb98f019a22778373cc197d560619
Arzal007 <145385320+Arzal007@users.noreply.github.com>
2024-04-16 13:53:30 +0500

    Update bawa.py

--- bawa.py
+++ bawa.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.core import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
